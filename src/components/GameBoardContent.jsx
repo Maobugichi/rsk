371cbef87adfc5745ee3a8ba000839427afcc9ab
Maobugichi <immansamuel846@gmail.com>
2025-01-27 04:17:33 -0800

    second commit

--- src/components/GameBoardContent.jsx
+++ src/components/GameBoardContent.jsx
@@ -1,7 +1,7 @@
 
 const GameBoardContent = ({Gameplay,GamePiece,animations,handleClick,score,setScore}) => {
     return(
-        <motiondiv className="flex  relative flex-wrap lg:w-1/2 w-[90%] md:w-[70%] justify-around items-center gap-20 h-auto lg:min-h-[85vh] large-square md:min-h-[40vh]  min-h-[50vh]">
+        <div className="flex  relative flex-wrap lg:w-1/2 w-[90%] md:w-[70%] justify-around items-center gap-20 h-auto lg:min-h-[85vh] large-square md:min-h-[40vh]  min-h-[50vh]">
         <svg  className="absolute " width="313" height="278" xmlns="http://www.w3.org/2000/svg"><path stroke="#000" stroke-width="15" fill="none" opacity=".2" d="M156.5 262 300 8H13z"/></svg>
             {
                 Gameplay.map((game,index) => {
